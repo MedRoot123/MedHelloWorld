44f07f088663946fa601313ee93bb32826cae90b
Taoufikchaouech <mohamedtaoufikchaouech@gmail.com>
2023-03-21 16:39:41 +0100

    modification message

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+
+
+/**
+ *
+ * @author Service_Tech
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+        System.out.println("salut mohamed taoufik!");
+        
+    }
+}
